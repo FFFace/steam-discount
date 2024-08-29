54ae1fe400916b1eb7a19bf483a864b3f51f1215
FFFace <59927358+FFFace@users.noreply.github.com>
2024-08-29 17:29:25 +0900

    feat: DiscountList Entity 추가

--- steam-discount/src/main/java/com/steam_discount/entity/discountList/DiscountList.java
+++ steam-discount/src/main/java/com/steam_discount/entity/discountList/DiscountList.java
@@ -1,6 +1,34 @@
 package com.steam_discount.entity.discountList;
 
 
-public class DiscountList {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
 
+@Entity
+@Getter
+@Table(name = "discount_list")
+public class DiscountList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String link;
+
+    @Column
+    private String originPrice;
+
+    @Column
+    private String discountPrice;
+
+    @Column
+    private String discountPercent;
 }
