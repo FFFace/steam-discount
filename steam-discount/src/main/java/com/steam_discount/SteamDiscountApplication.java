87adc0b9dbbdf7cbbfe9321296aafdf04dc0eccb
FFFace <59927358+FFFace@users.noreply.github.com>
2024-08-29 19:52:01 +0900

    feat: security off

--- steam-discount/src/main/java/com/steam_discount/SteamDiscountApplication.java
+++ steam-discount/src/main/java/com/steam_discount/SteamDiscountApplication.java
@@ -2,8 +2,9 @@ package com.steam_discount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SteamDiscountApplication {
 
 	public static void main(String[] args) {
