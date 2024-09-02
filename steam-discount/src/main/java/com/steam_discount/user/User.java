b41ca3ea691de7f8f30fa4e9c50cb4c23cd892a7
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-02 12:44:38 +0900

    feat: user entity 추가

---
+++ steam-discount/src/main/java/com/steam_discount/user/User.java
@@ -0,0 +1,37 @@
+package com.steam_discount.user;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.checkerframework.checker.units.qual.C;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String product;
+
+    @Column
+    private String productId;
+}
