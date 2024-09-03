558da923d69ef867de470c7ed030204164f3bad9
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-03 15:38:28 +0900

    feat: UserService 추가

---
+++ steam-discount/src/main/java/com/steam_discount/user/service/UserService.java
@@ -0,0 +1,28 @@
+package com.steam_discount.user.service;
+
+import com.steam_discount.common.exception.CustomException;
+import com.steam_discount.common.exception.errorCode.ErrorCode;
+import com.steam_discount.user.entity.User;
+import com.steam_discount.user.repository.UserRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public User findUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() ->
+            new CustomException(ErrorCode.NOT_FOUND_USER));
+    }
+}
