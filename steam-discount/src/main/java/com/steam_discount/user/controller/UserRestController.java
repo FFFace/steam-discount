adcb62c776836c6e1651c4a77519c13bae89ddbc
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-03 15:42:40 +0900

    feat: 사용자 검색 요청 추가

--- steam-discount/src/main/java/com/steam_discount/user/controller/UserRestController.java
+++ steam-discount/src/main/java/com/steam_discount/user/controller/UserRestController.java
@@ -1,9 +1,30 @@
 package com.steam_discount.user.controller;
 
 
+import com.steam_discount.user.entity.User;
+import com.steam_discount.user.service.UserService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserRestController {
 
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUserList(){
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@RequestParam Long id){
+        return ResponseEntity.ok(userService.findUserById(id));
+    }
 }
