afef6f75cd2554a26af80e4ee57d7b66ca966795
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-03 16:43:16 +0900

    feat: UserDTO 추가

---
+++ steam-discount/src/main/java/com/steam_discount/user/entity/UserDTO.java
@@ -0,0 +1,17 @@
+package com.steam_discount.user.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserDTO {
+    private String email;
+    private String password;
+    private String nickname;
+    private String product;
+    private String productId;
+}
