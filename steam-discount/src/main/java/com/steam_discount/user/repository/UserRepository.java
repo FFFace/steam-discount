5806a2e08336bf1a8fbdde6ff7c5907c90d8c716
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-03 14:31:28 +0900

    fix: Repository 어노테이션 추가

--- steam-discount/src/main/java/com/steam_discount/user/repository/UserRepository.java
+++ steam-discount/src/main/java/com/steam_discount/user/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.steam_discount.user.repository;
 
 import com.steam_discount.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
