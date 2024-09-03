44e1db24b06be084b3584572c3d4ac1a55a83246
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-03 18:55:00 +0900

    feat: user post 추가

--- steam-discount/src/main/java/com/steam_discount/user/controller/UserRestController.java
+++ steam-discount/src/main/java/com/steam_discount/user/controller/UserRestController.java
@@ -2,11 +2,17 @@ package com.steam_discount.user.controller;
 
 
 import com.steam_discount.user.entity.User;
+import com.steam_discount.user.entity.UserDTO;
 import com.steam_discount.user.service.UserService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +32,10 @@ public class UserRestController {
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@RequestParam Long id){
         return ResponseEntity.ok(userService.findUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody UserDTO userDTO){
+        return new ResponseEntity<>(userService.saveNewUser(userDTO), HttpStatus.CREATED);
     }
 }
