8cf965cb9507163943232f656c87356ae8bc9315
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-03 16:42:25 +0900

    feat: ExceptionHandler 추가

--- steam-discount/src/main/java/com/steam_discount/common/exception/GlobalExceptionHandler.java
+++ steam-discount/src/main/java/com/steam_discount/common/exception/GlobalExceptionHandler.java
@@ -1,8 +1,24 @@
 package com.steam_discount.common.exception;
 
+import com.steam_discount.common.exception.errorCode.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ResponseException> customExceptionHandler(CustomException exception){
+        ResponseException responseException = exception.toResponse();
+        ErrorCode errorCode = responseException.getErrorCode();
+        log.error("### ERROR!! CODE: {}        CODE NAME: {}        CODE MESSAGE: {}",
+                    errorCode.getErrorCode(), errorCode.getErrorName(), errorCode.getErrorMessage());
+
+        return new ResponseEntity<>(responseException, HttpStatusCode.valueOf(errorCode.getErrorCode()));
+    }
 }
