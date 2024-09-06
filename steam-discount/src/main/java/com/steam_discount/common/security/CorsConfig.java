78ad21f2c281891744f14858b54f601586c9d439
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-06 18:20:51 +0900

    fix: Cors 설정 수정

--- steam-discount/src/main/java/com/steam_discount/common/security/CorsConfig.java
+++ steam-discount/src/main/java/com/steam_discount/common/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOriginPatterns(
-            List.of("http://localhost", "http://"));
+            List.of("http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
