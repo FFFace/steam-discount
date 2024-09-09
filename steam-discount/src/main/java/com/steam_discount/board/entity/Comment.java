7c1f0e77f11c76cfd2d19554f414e470b3a4c5ea
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-09 15:49:06 +0900

    feat: comment entity 추가

---
+++ steam-discount/src/main/java/com/steam_discount/board/entity/Comment.java
@@ -0,0 +1,45 @@
+package com.steam_discount.board.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column
+    private String writer;
+
+    @Column
+    private String name;
+
+    @Column
+    private String content;
+
+    @Column
+    private Integer thumbsUp;
+
+    @Column
+    private Integer thumbsDown;
+}
