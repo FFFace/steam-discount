18a84990766d19ffc6b840643694897a2ea0c8ed
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-09 15:09:18 +0900

    feat: Board entity 추가

---
+++ steam-discount/src/main/java/com/steam_discount/board/entity/Board.java
@@ -0,0 +1,31 @@
+package com.steam_discount.board.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "board")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+}
