9c0b802aa02ec953d36bc0b40e4dba4accb3550f
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-10 14:36:11 +0900

    feat: baseentity 추가

---
+++ steam-discount/src/main/java/com/steam_discount/common/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.steam_discount.common.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity {
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    public void prePersist(){
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+}
