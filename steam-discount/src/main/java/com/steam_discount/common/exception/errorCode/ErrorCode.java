d589746442e73afa254889de93200f3d985363b5
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-12 18:19:24 +0900

    feat: 인증 실패 에러코드 추가

--- steam-discount/src/main/java/com/steam_discount/common/exception/errorCode/ErrorCode.java
+++ steam-discount/src/main/java/com/steam_discount/common/exception/errorCode/ErrorCode.java
@@ -7,8 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    NOT_FOUND_USER(401, "잘못된 요청", "해당 사용자를 찾을 수 없습니다.");
-
+    NOT_FOUND_USER(400, "잘못된 요청", "해당 사용자를 찾을 수 없습니다."),
+    NOT_VERIFY_EMAIL(403, "인증되지 않음", "아직 인증되지 않은 이메일입니다."),
+    NOT_FOUND_ALGORITHM(500, "알고리즘 없음", "알고리즘을 찾을 수 없습니다.");
 
     private final int errorCode;
     private final String errorName;
