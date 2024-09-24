b1ef6292202775a7fb8ee1a54a72ec3b244bfc85
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-24 22:37:33 +0900

    feat: 회원가입 중복 검사

--- steam-discount/src/main/java/com/steam_discount/user/service/UserService.java
+++ steam-discount/src/main/java/com/steam_discount/user/service/UserService.java
@@ -2,16 +2,21 @@ package com.steam_discount.user.service;
 
 import com.steam_discount.common.exception.CustomException;
 import com.steam_discount.common.exception.errorCode.ErrorCode;
+import com.steam_discount.common.security.jwt.JwtUtil;
 import com.steam_discount.common.smtp.MailService;
+import com.steam_discount.user.entity.Login;
 import com.steam_discount.user.entity.User;
 import com.steam_discount.user.entity.UserDTO;
 import com.steam_discount.user.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.List;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,12 +24,21 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
     private static final String AUTO_CODE_PREFIX = "AuthCode ";
     private final MailService mailService;
+    private PasswordEncoder passwordEncoder;
 
     @Value("${spring.mail.auth-code-expiration-millis}")
     private Long authCodeExpirationMillis;
 
+    private String passwordEncode(String password){
+        return passwordEncoder.encode(password);
+    }
+
+    private boolean passwordMatch(User user, String password){
+        return passwordEncoder.matches(password, user.getPassword());
+    }
 
     public List<User> findAllUsers(){
         return userRepository.findAll();
@@ -39,6 +53,7 @@ public class UserService {
         String code = getVerifyCodeNSendCodeToEmail(userDTO);
         User user = userDTO.toEntity();
         user.setVerifyCode(code);
+        user.setPassword(passwordEncode(user.getPassword()));
         save(user);
     }
 
@@ -97,5 +112,17 @@ public class UserService {
 
     public void deleteUser(User user){
         userRepository.delete(user);
+    }
+
+    public void login(Login login, HttpServletResponse response){
+        User user = userRepository.findByEmail(login.getEmail()).orElseThrow(()->
+            new CustomException(ErrorCode.NOT_FOUND_USER));
+
+        if(!passwordMatch(user, login.getPassword()))
+            throw new CustomException(ErrorCode.NOT_MATCH_PASSWORD);
+
+        String accessToken = jwtUtil.generateAccessToken(user.getEmail());
+
+
     }
 }
