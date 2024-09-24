e3d56afbcc00e0069d85328f18f52fff1437e135
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-24 22:36:51 +0900

    feat: login entity 추가

---
+++ steam-discount/src/main/java/com/steam_discount/user/entity/Login.java
@@ -0,0 +1,16 @@
+package com.steam_discount.user.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Login {
+    private String email;
+    private String password;
+}
