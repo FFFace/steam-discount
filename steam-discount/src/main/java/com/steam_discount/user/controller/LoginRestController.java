00c4ab54bafd1c2fc9a667db075bb4b2db9b84ba
FFFace <59927358+FFFace@users.noreply.github.com>
2024-09-25 20:34:23 +0900

    feat: LoginRestController 추가

---
+++ steam-discount/src/main/java/com/steam_discount/user/controller/LoginRestController.java
@@ -0,0 +1,31 @@
+package com.steam_discount.user.controller;
+
+
+import com.steam_discount.common.security.jwt.user.CustomUser;
+import com.steam_discount.user.entity.Login;
+import com.steam_discount.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class LoginRestController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public void login(@RequestBody Login login, HttpServletResponse response){
+        userService.login(login, response);
+    }
+
+    @PostMapping("/logout")
+    public void logout(@AuthenticationPrincipal CustomUser customUser){
+        userService.logout(customUser.getUser());
+    }
+}
