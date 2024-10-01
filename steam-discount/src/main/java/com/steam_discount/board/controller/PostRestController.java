04795bc2d046ebbff220754c9849815a5e27b657
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-01 20:26:26 +0900

    feat: 특정 post 요청 추가

--- steam-discount/src/main/java/com/steam_discount/board/controller/PostRestController.java
+++ steam-discount/src/main/java/com/steam_discount/board/controller/PostRestController.java
@@ -1,19 +1,24 @@
 package com.steam_discount.board.controller;
 
 
-import com.steam_discount.board.entity.Post;
 import com.steam_discount.board.entity.dto.PostDTO;
 import com.steam_discount.board.entity.dto.PostPageDTO;
+import com.steam_discount.board.entity.responseDTO.PostPageResponseDTO;
 import com.steam_discount.board.entity.responseDTO.PostResponseDTO;
 import com.steam_discount.board.service.PostService;
+import com.steam_discount.common.security.jwt.user.CustomUser;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.jcache.interceptor.JCacheOperationSource;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,7 +31,17 @@ public class PostRestController {
 
 
     @GetMapping
-    public ResponseEntity<List<PostResponseDTO>> getPosts(@RequestBody @Valid PostPageDTO postPageDTO){
+    public ResponseEntity<List<PostPageResponseDTO>> getPosts(@RequestBody @Valid PostPageDTO postPageDTO){
         return ResponseEntity.ok(postService.findPostList(postPageDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDTO> getPost(@PathVariable Long id){
+        return ResponseEntity.ok(postService.findPostById(id));
+    }
+
+    @PostMapping
+    public void createPost(@RequestBody @Valid PostDTO postDTO, @AuthenticationPrincipal CustomUser customUser){
+        postService.createPost(postDTO, customUser.getUser());
     }
 }
--- steam-discount/src/main/java/com/steam_discount/board/service/PostService.java
+++ steam-discount/src/main/java/com/steam_discount/board/service/PostService.java
@@ -5,19 +5,17 @@ import com.steam_discount.board.entity.Board;
 import com.steam_discount.board.entity.Post;
 import com.steam_discount.board.entity.dto.PostDTO;
 import com.steam_discount.board.entity.dto.PostPageDTO;
+import com.steam_discount.board.entity.responseDTO.PostPageResponseDTO;
 import com.steam_discount.board.entity.responseDTO.PostResponseDTO;
 import com.steam_discount.board.repository.BoardRepository;
 import com.steam_discount.board.repository.PostRepository;
 import com.steam_discount.common.exception.CustomException;
 import com.steam_discount.common.exception.errorCode.ErrorCode;
 import com.steam_discount.user.entity.User;
-import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,24 +25,30 @@ public class PostService {
     private final PostRepository postRepository;
     private final BoardRepository boardRepository;
 
-
-
-
-    public List<PostResponseDTO> findPostList(PostPageDTO postPageDTO){
+    public List<PostPageResponseDTO> findPostList(PostPageDTO postPageDTO){
         Board board = boardRepository.findById(postPageDTO.getBoardId()).orElseThrow(() ->
             new CustomException(ErrorCode.NOT_FOUND_BOARD));
 
-        PageRequest pageRequest = PageRequest.of(postPageDTO.getPage(), 10);
+        int pageSize = 10;
+
+        PageRequest pageRequest = PageRequest.of(postPageDTO.getPage(), pageSize);
         List<Post> postList = postRepository.findByBoardId(postPageDTO.getBoardId(), pageRequest);
-        List<PostResponseDTO> postResponseDTOList = new ArrayList<>();
+        List<PostPageResponseDTO> postPageResponseDTOList = new ArrayList<>();
         for(Post post : postList){
-            postResponseDTOList.add(post.toResponseDTO());
+            postPageResponseDTOList.add(post.toPageResponseDTO());
         }
 
-        return postResponseDTOList;
+        return postPageResponseDTOList;
     }
 
-    public void savePost(PostDTO postDTO, User user){
+    public PostResponseDTO findPostById(Long id){
+        Post post = postRepository.findById(id).orElseThrow(() ->
+            new CustomException(ErrorCode.NOT_FOUND_POST));
+
+        return post.toPostResponseDTO();
+    }
+
+    public void createPost(PostDTO postDTO, User user){
         Board board = boardRepository.findById(postDTO.getBoardId()).orElseThrow(() ->
             new CustomException(ErrorCode.NOT_FOUND_BOARD));
 
