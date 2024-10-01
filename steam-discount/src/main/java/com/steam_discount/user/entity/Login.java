7a86f181ebf47471b1d42e0c32945043f6e8fa31
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-01 20:22:36 +0900

    feat: 검증 추가

--- steam-discount/src/main/java/com/steam_discount/user/entity/Login.java
+++ steam-discount/src/main/java/com/steam_discount/user/entity/Login.java
@@ -1,6 +1,9 @@
 package com.steam_discount.user.entity;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Login {
+    @NotBlank(message = "이메일이 옳바르지 않습니다.")
+    @Email(message = "이메일이 옳바르지 않습니다.")
     private String email;
+    @NotBlank(message = "패스워드가 옳바르지 않습니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}", message = "패스워드가 옳바르지 않습니다.")
     private String password;
 }
