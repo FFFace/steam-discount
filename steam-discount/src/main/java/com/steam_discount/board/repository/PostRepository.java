06aae47d439703ae49652f2f176ad6f4f2447f70
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-01 17:23:04 +0900

    feat: 특정 게시판 게시글 페이징 요청

--- steam-discount/src/main/java/com/steam_discount/board/repository/PostRepository.java
+++ steam-discount/src/main/java/com/steam_discount/board/repository/PostRepository.java
@@ -1,11 +1,18 @@
 package com.steam_discount.board.repository;
 
+import com.steam_discount.board.entity.Board;
 import com.steam_discount.board.entity.Post;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query(value = "select * from post where board_id=:boardId limit :startIndex, 10", nativeQuery = true)
+    List<Post> findByBoardIdAndLimit10(Long boardId, Integer startIndex);
 
+    List<Post> findByBoardId(Long boardId, Pageable pageable);
 }
