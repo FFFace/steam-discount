e73879d4aad125364923e8c79699e87cc9f1f184
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-01 19:28:22 +0900

    feat: 게시글 에러 추가

--- steam-discount/src/main/java/com/steam_discount/common/exception/errorCode/ErrorCode.java
+++ steam-discount/src/main/java/com/steam_discount/common/exception/errorCode/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     NOT_FOUND_REFRESH_TOKEN(400, "잘못된 요청", "로그인 중인 사용자가 아닙니다."),
     NOT_MATCH_PASSWORD(400, "잘못된 요청", "비밀번호가 일치하지 않습니다."),
     NOT_FOUND_BOARD(400, "잘못된 요청", "존재하지 않는 게시판 입니다."),
+    NOT_FOUND_POST(400, "잘못된 요청", "존재하지 않는 게시글 입니다."),
     NOT_VERIFY_EMAIL(403, "인증되지 않음", "아직 인증되지 않은 이메일입니다."),
     NOT_FOUND_ALGORITHM(500, "알고리즘 없음", "알고리즘을 찾을 수 없습니다."),
     ACCESS_TOKEN_EXPIRED(401, "토큰 만료", "엑세스 토큰이 만료되었습니다.");
