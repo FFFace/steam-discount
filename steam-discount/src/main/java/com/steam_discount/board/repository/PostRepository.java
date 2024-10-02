099b09a7abc0d2e062caa18da53b147dee1e0593
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-02 22:34:43 +0900

    feat: 메인 페이지 공지사항 추가

--- steam-discount/src/main/java/com/steam_discount/board/repository/PostRepository.java
+++ steam-discount/src/main/java/com/steam_discount/board/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.steam_discount.board.repository;
 import com.steam_discount.board.entity.Board;
 import com.steam_discount.board.entity.Post;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,7 +14,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     @Query(value = "select * from post where board_id=:boardId limit :startIndex, 10", nativeQuery = true)
-    List<Post> findByBoardIdAndLimit10(Long boardId, Integer startIndex);
+    List<Post> findByBoardIdAndLimit10(long boardId, Integer startIndex);
 
-    Page<Post> findByBoardId(Long boardId, Pageable pageable);
+    Page<Post> findByBoardId(long boardId, Pageable pageable);
+    Optional<Post> findLastByBoardId(long boardId);
 }
