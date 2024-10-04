038f43c26f91ede10b6ca249f39e742eac298c34
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-04 14:33:33 +0900

    feat: PostThumbsRepository 추가

---
+++ steam-discount/src/main/java/com/steam_discount/board/repository/PostThumbsRepository.java
@@ -0,0 +1,8 @@
+package com.steam_discount.board.repository;
+
+import com.steam_discount.board.entity.PostThumbs;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostThumbsRepository extends JpaRepository<PostThumbs, Long> {
+
+}
