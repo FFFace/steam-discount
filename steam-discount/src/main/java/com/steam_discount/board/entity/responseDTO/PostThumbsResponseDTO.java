2172cc35234d4a3bbab84f276888d0ad025faeb5
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-04 15:45:33 +0900

    feat: PostThumbsResponseDTO 추가

---
+++ steam-discount/src/main/java/com/steam_discount/board/entity/responseDTO/PostThumbsResponseDTO.java
@@ -0,0 +1,16 @@
+package com.steam_discount.board.entity.responseDTO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostThumbsResponseDTO {
+    private int thumbsUp;
+    private int thumbsDown;
+}
