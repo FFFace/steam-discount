e0835833ff03ddc080d2f28e16e72874783ecdbf
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-26 00:39:21 +0900

    fix: cors 추가

--- steam-discount/src/main/java/com/steam_discount/common/security/CorsConfig.java
+++ steam-discount/src/main/java/com/steam_discount/common/security/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOriginPatterns(
-            List.of("http://3.82.176.63", "http://localhost"));
+            List.of("http://3.82.176.63", "http://localhost", "http://ec2-3-82-176-63.compute-1.amazonaws.com/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
