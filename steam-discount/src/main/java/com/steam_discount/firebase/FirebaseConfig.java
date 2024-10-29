f4528a087518d88a27154283c69fd6998f0a30d2
FFFace <59927358+FFFace@users.noreply.github.com>
2024-10-29 23:54:34 +0900

    fix: 파이어베이스 수정

--- steam-discount/src/main/java/com/steam_discount/firebase/FirebaseConfig.java
+++ steam-discount/src/main/java/com/steam_discount/firebase/FirebaseConfig.java
@@ -1,11 +1,12 @@
 package com.steam_discount.firebase;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,9 +16,13 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${firebase.service-account.path}")
+    private String serviceAccountPath;
+
     @Bean
     public Storage storage() throws IOException {
-        try (FileInputStream serviceAccount = new FileInputStream("./firebase_service_account.json")) {
+        try (FileInputStream serviceAccount = new FileInputStream(serviceAccountPath)) {
             GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
             return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
         }
@@ -25,7 +30,7 @@ public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-        try (FileInputStream serviceAccount = new FileInputStream("./firebase_service_account.json")) {
+        try (FileInputStream serviceAccount = new FileInputStream(serviceAccountPath)) {
             FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
