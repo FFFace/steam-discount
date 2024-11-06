dc06691bd34cd7eb333d07a47431637b88b2ab65
FFFace <59927358+FFFace@users.noreply.github.com>
2024-11-06 19:36:12 +0900

    fix: seleinum 버전 수정

--- steam-discount/build.gradle.kts
+++ steam-discount/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	implementation("org.seleniumhq.selenium:selenium-java:4.24.0")
+	implementation("org.seleniumhq.selenium:selenium-java:4.26.0")
 	implementation("com.google.firebase:firebase-admin:9.4.1")
 
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
