9db9e64f9edac354a0791f791a2b3dd47e78287b
FFFace <59927358+FFFace@users.noreply.github.com>
2024-11-14 10:14:29 +0900

    feat: 크롤링 스케쥴러

--- steam-discount/src/main/java/com/steam_discount/SteamDiscountApplication.java
+++ steam-discount/src/main/java/com/steam_discount/SteamDiscountApplication.java
@@ -3,8 +3,10 @@ package com.steam_discount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableScheduling
 public class SteamDiscountApplication {
 
 	public static void main(String[] args) {
