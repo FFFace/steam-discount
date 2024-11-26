1e99f70d2248a337e43f583d0b901e7aebcfaa69
FFFace <59927358+FFFace@users.noreply.github.com>
2024-11-26 15:13:34 +0900

    build: oauth2 추가

--- steam-discount/build.gradle.kts
+++ steam-discount/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-mail:3.3.3")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
 	implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
