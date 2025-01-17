d77dfe993d20ff3727b90b830a52b90d36d2ed07
FFFace <59927358+FFFace@users.noreply.github.com>
2025-01-18 04:57:13 +0900

    fix

--- steam-discount/src/main/java/com/steam_discount/SteamDiscountApplication.java
+++ steam-discount/src/main/java/com/steam_discount/SteamDiscountApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableScheduling
 @EnableRetry
+@EnableAsync
 public class SteamDiscountApplication {
 
 	public static void main(String[] args) {
